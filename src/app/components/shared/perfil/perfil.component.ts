4a879b623f647f5ea4df0e157dfb650ea9e09a37
daniel gonzalo alzate restrepo <d.alzate598@gmail.com>
2024-03-03 22:04:27 -0500

    avances portafolio

--- src/app/components/shared/perfil/perfil.component.ts
+++ src/app/components/shared/perfil/perfil.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { PortalModule } from '@angular/cdk/portal';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CardModule, PortalModule, DragDropModule ],
+  imports: [CardModule, PortalModule, DragDropModule,CommonModule ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
 export class PerfilComponent implements OnInit{
 
+  angularclick:boolean = true;
+  laravelclick:boolean = false;
 
   constructor( ){}
 
@@ -19,7 +22,13 @@ export class PerfilComponent implements OnInit{
     // this.prueba();
   }
  
-
-  
+  angular(){
+    this.angularclick = true;
+    this.laravelclick = false;
+  }
+  laravel(){
+    this.laravelclick = true;
+    this.angularclick = false;
+  }
 
 }
