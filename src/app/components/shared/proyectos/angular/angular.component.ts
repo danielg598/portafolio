59eb91258153ef836450db412fcce47fb414407c
dgalzater <dgalzater@gmail.com>
2024-03-28 18:56:15 -0500

    se crea servicio para modal overlay y se ajusta el componente de proyectos

--- src/app/components/shared/proyectos/angular/angular.component.ts
+++ src/app/components/shared/proyectos/angular/angular.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalOverlayService } from '../../../../utilities/services/modal-overlay.service';
+import { AngularInfoAppGiffsComponent } from '../../../../utilities/Modales/angular-info-app-giffs/angular-info-app-giffs.component';
+import { PortalModule } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-angular',
   standalone: true,
-  imports: [],
+  imports: [
+    AngularInfoAppGiffsComponent,
+    PortalModule
+  ],
   templateUrl: './angular.component.html',
   styleUrl: './angular.component.scss'
 })
 export class AngularComponent {
 
-  constructor(){}
+  @ViewChild('InfoAppGiff') InfoAppGiff: any;
+
+  constructor(private modal:ModalOverlayService){}
+
+  infoAppGiffs(){
+    this.modal.openOverlay(this.InfoAppGiff)
+    console.log("hola mundo");
+    
+  }
 
 }
---
+++ src/app/utilities/Modales/angular-info-app-giffs/angular-info-app-giffs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-info-app-giffs',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-info-app-giffs.component.html',
+  styleUrl: './angular-info-app-giffs.component.scss'
+})
+export class AngularInfoAppGiffsComponent {
+
+}
