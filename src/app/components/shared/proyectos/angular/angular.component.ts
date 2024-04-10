3fb094ff05005caa34394ec9b4c2bdee9172b228
Daniel Alzate <d.alzate598@gmail.com>
2024-04-09 22:22:57 -0500

    avances

--- src/app/components/shared/proyectos/angular/angular.component.ts
+++ src/app/components/shared/proyectos/angular/angular.component.ts
@@ -3,12 +3,14 @@ import { Router } from '@angular/router';
 import { ModalOverlayService } from '../../../../utilities/services/modal-overlay.service';
 import { AngularInfoAppGiffsComponent } from '../../../../utilities/Modales/angular-info-app-giffs/angular-info-app-giffs.component';
 import { PortalModule } from '@angular/cdk/portal';
+import { FiltroAppComponent } from '../../../../utilities/Modales/filtro-app/filtro-app.component';
 
 @Component({
   selector: 'app-angular',
   standalone: true,
   imports: [
     AngularInfoAppGiffsComponent,
+    FiltroAppComponent,
     PortalModule
   ],
   templateUrl: './angular.component.html',
@@ -17,13 +19,16 @@ import { PortalModule } from '@angular/cdk/portal';
 export class AngularComponent {
 
   @ViewChild('InfoAppGiff') InfoAppGiff: any;
+  @ViewChild('InfoAppFiltrador') InfoAppFiltrador: any;
 
   constructor(private modal:ModalOverlayService){}
 
   infoAppGiffs(){
-    this.modal.openOverlay(this.InfoAppGiff)
-    console.log("hola mundo");
-    
+    this.modal.openOverlay(this.InfoAppGiff);
+
+  }
+  filterPipe(){
+    this.modal.openOverlay(this.InfoAppFiltrador);
   }
 
 }
---
+++ src/app/utilities/Modales/filtro-app/filtro-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './filtro-app.component.html',
+  styleUrl: './filtro-app.component.scss'
+})
+export class FiltroAppComponent {
+
+}
