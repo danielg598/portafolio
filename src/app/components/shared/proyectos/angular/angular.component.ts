b95db876ed6fee733870220b665ad23948064752
Daniel Alzate <d.alzate598@gmail.com>
2024-04-14 21:06:19 -0500

    creación proyecto de form app

--- src/app/components/shared/proyectos/angular/angular.component.ts
+++ src/app/components/shared/proyectos/angular/angular.component.ts
@@ -4,6 +4,7 @@ import { ModalOverlayService } from '../../../../utilities/services/modal-overla
 import { AngularInfoAppGiffsComponent } from '../../../../utilities/Modales/angular-info-app-giffs/angular-info-app-giffs.component';
 import { PortalModule } from '@angular/cdk/portal';
 import { FiltroAppComponent } from '../../../../utilities/Modales/filtro-app/filtro-app.component';
+import { FormAppModalComponent } from '../../../../utilities/Modales/form-app-modal/form-app-modal.component';
 
 @Component({
   selector: 'app-angular',
@@ -11,6 +12,7 @@ import { FiltroAppComponent } from '../../../../utilities/Modales/filtro-app/fil
   imports: [
     AngularInfoAppGiffsComponent,
     FiltroAppComponent,
+    FormAppModalComponent,
     PortalModule
   ],
   templateUrl: './angular.component.html',
@@ -20,6 +22,7 @@ export class AngularComponent {
 
   @ViewChild('InfoAppGiff') InfoAppGiff: any;
   @ViewChild('InfoAppFiltrador') InfoAppFiltrador: any;
+  @ViewChild('InfoAppFormApp') InfoAppFormApp: any;
 
   constructor(private modal:ModalOverlayService){}
 
@@ -30,5 +33,7 @@ export class AngularComponent {
   filterPipe(){
     this.modal.openOverlay(this.InfoAppFiltrador);
   }
-
+  formApp(){
+    this.modal.openOverlay(this.InfoAppFormApp);
+  }
 }
---
+++ src/app/utilities/Modales/form-app-modal/form-app-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-app-modal.component.html',
+  styleUrl: './form-app-modal.component.scss'
+})
+export class FormAppModalComponent {
+
+}
