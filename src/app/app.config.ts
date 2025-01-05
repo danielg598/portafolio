3fb941459657e75a2130f1be8619a71f8c704358
daniel alzate <d.alzate598gmail.com>
2025-01-05 18:07:18 -0500

    actualizacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,19 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { providePrimeNG } from '../node_modules/prime';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    providePrimeNG({
+      theme: {
+          preset: Aura
+      }
+  })
+  ]
 };
