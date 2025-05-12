1ee997d69a96a4784b4830c956e76eb672b5b946
daniel alzate <d.alzate598gmail.com>
2025-05-12 11:45:25 -0500

    actualizacion componente de contacto icono de carga

--- src/app/components/shared/contacto/contacto.component.ts
+++ src/app/components/shared/contacto/contacto.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ContactoService } from '../../../utilities/services/contacto.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-contacto',
@@ -13,6 +14,7 @@ export class ContactoComponent {
 
   enviado = false;
   error = '';
+  cargando: boolean = false;
 
   public empresaForm: FormGroup = this.fb.group({
     nombreEmpresa: ['', [Validators.required]],
@@ -30,19 +32,25 @@ export class ContactoComponent {
   onSubmit() {
     if (this.empresaForm.valid) {
       this.empresaForm.markAllAsTouched();
+      this.cargando = true;
       console.log("valor formulario", this.empresaForm.value);
 
-      this.contactoService.enviarFormulario(this.empresaForm.value).subscribe({
-        next: () => {
-          this.enviado = true;
-          this.error = '';
-          this.empresaForm.reset();
-        },
-        error: (err) => {
-          this.error = err.message;
-          this.enviado = false;
-        }
-      });
+      this.contactoService.enviarFormulario(this.empresaForm.value)
+        .pipe(
+          finalize(() => {
+            this.cargando = false;
+          })
+        ).subscribe({
+          next: () => {
+            this.enviado = true;
+            this.empresaForm.reset();
+          },
+          error: (err) => {
+            this.error = err.message;
+          }
+        });
+
+
 
 
     }
--- src/app/utilities/environments/environment.ts
+++ src/app/utilities/environments/environment.ts
@@ -2,3 +2,4 @@ export const environment ={
   production:false,
   api: 'https://localhost:7063/api/'
 }
+// https://localhost:7063/api/
